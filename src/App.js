8be2b7d925c6b8e27e348fd17feb8a5bbf1f4229
PhuongLe1301 <phuongle130199@gmail.com>
2022-05-24 15:41:33 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './test';
 
 function App() {
+  const m1 = [0, 10, 1, 99, 9, 8, 79, 91, 22, 32, 12]
+  m1.sort((a, b) => b - a);
+  const m2 = [99, 19, 29, 39, 11, 21, 32, 33, 35, 50, 60, 90]
+  m2.sort((a, b) => b - a);
+  const m3 = [1, 10, 19, 11, 13, 16, 19]
+  m3.sort((a, b) => b - a);
+  console.log('Mảng1 ' + m1.join(''))
+  console.log('Mảng2 ' + m2.join(''))
+  console.log('Mảng3 ' + m3.join(''))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Test/>
   );
 }
 
